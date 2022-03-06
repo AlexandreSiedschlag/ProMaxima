63373fd386271d16be67547c2af002588aed9b6b
Alexandre Siedschlag <88800338+AlexandreSiedschlag@users.noreply.github.com>
2022-03-06 16:22:00 -0300

    SQLite db inpector, DB saving correctly

--- ThisIsTheProject/app1/views.py
+++ ThisIsTheProject/app1/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .functions import do_something
 
 def home(response):
@@ -7,5 +7,5 @@ def home(response):
 
 def index(response):
     do_something()
-    return render(response, 'app1/home.html')
+    return HttpResponse(response, 'http://127.0.0.1:8000/')
 # Create your views here.
